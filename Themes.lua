1978afdd29442166f710f55c5be80258b9a1a432
AstroX <dreddthedeath@gmail.com>
2024-12-13 18:35:06 +0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -1,14 +1,11 @@
 local Themes = {
   ["Default"] = {
-    ["Color Hub 1"] = ColorSequence.new({
-        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(28, 23, 25.5),
-        ColorSequenceKeypoint.new(0.50, Color3.fromRGB(30.5, 30.5, 30.5),
-        ColorSequenceKeypoint.new(1.00, Color3.fromRGB(28, 23, 25.5),
+    ["Color Hub 1"] = Color3.fromRGB(28, 23, 25.5),
     ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
     ["Color Theme"] = Color3.fromRGB(255, 0, 0),
     ["Color Text"] = Color3.fromRGB(240, 240, 240),
     ["Color Dark Text"] = Color3.fromRGB(180, 180, 180),
-    ["Corner Radius"] = UDim.new(0, 5),
+    ["Corner Radius"] = UDim.new(0, 7),
     ["Font"] = {
       Enum.Font.GothamBlack,
       Enum.Font.GothamBold,
