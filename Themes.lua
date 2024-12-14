29c50522c4bb11a12dc022990c891274a40d8f9d
AstroX <dreddthedeath@gmail.com>
2024-12-14 17:08:23 +0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -1,7 +1,11 @@
 local Themes = {
   ["Default"] = {
-    ["Color Hub 1"] = Color3.fromRGB(28, 23, 25.5),
-    ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
+    ["Color Hub 1"] = ColorSequence.new({
+        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(28, 23, 25.5)),
+        ColorSequenceKeypoint.new(0.50, Color3.fromRGB(30.5, 30.5, 30.5)),
+        ColorSequenceKeypoint.new(1.00, Color3.fromRGB(28, 23, 25.5))
+    }),
+    ["Color Hub 2"] = Color3.fromRGB(28, 28, 28),
     ["Color Theme"] = Color3.fromRGB(255, 0, 0),
     ["Color Text"] = Color3.fromRGB(240, 240, 240),
     ["Color Dark Text"] = Color3.fromRGB(180, 180, 180),
@@ -17,3 +21,6 @@ local Themes = {
 }
 
 return Themes
+
+
+		 
