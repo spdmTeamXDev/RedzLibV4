fd8d74d1d1846dc8ed4c842ceea2c899fc59cf2e
SpdmTeamX <zusdrodo@gmail.com>
2025-05-29 04:09:31 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -20,7 +20,7 @@ local redzLib = {
     ScrollSize = 160,
     TransparencyHub = 0.1
   },
-  Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/AstroXTeam/RedzLibV4/refs/heads/main/Icons.lua"))() --[[
+  Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/spdmTeamXDev/RedzLibV4/refs/heads/main/Icons.lua"))() --[[
     I found this source on fluent but I don't know who created it
     source -- https://github.com/dawid-scripts/Fluent/blob/master/src/Icons.lua --
   ]],
